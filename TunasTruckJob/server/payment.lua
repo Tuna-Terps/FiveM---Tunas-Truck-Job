937dc2b7e4fe529452af5c4351243013a09d4a70
Tuna Terps <62733984+Tuna-Terps@users.noreply.github.com>
2021-08-03 03:27:33 -0400

    Update payment.lua

--- TunasTruckJob/server/payment.lua
+++ TunasTruckJob/server/payment.lua
@@ -1,22 +1,23 @@
 ESX = nil
-payMin = 200 
+payMin = 350 
 payMax = 500
 
 TriggerEvent("esx:getSharedObject", function(obj) ESX = obj end)
 
 RegisterNetEvent('TunasTruckJob:pay')
 AddEventHandler('TunasTruckJob:pay', function()
+    print('paying ...')
     local _source = source
     local xPlayer = ESX.GetPlayerFromId(_source)
         if xPlayer ~= nil then
             local randomMoney = math.random(payMin,payMax)
             xPlayer.addMoney(randomMoney)
             local cash = xPlayer.getMoney()
-		if randomMoney > 501 then
-			print(source .. " may be cheating ....")
-			return
-        	end
             TriggerClientEvent('banking:updateCash', _source, cash)
             TriggerClientEvent('esx:showNotification', _source,'You were paid $ '.. randomMoney)
         end
+end)
+-- stop trucking; thanks Soubisan <3
+RegisterCommand("stoptruck", function(source, args) 
+    TriggerClientEvent('TunasTruckJob_gw:stoptruck', source)
 end)
